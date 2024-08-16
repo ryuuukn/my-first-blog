747bc055e0763943d1fc0042b11f1b0fe39bd172
ryuuukn <r.kazuta.24.3.1306@4nonome.com>
2024-08-17 01:28:59 +0900

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,18 @@
 from .forms import PostForm
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import redirect
+
 
 def post_list(request):
-    posts = Post.objects.all().order_by('published_at')
+    posts = Post.objects.filter(published_at__lte=timezone.now()).order_by('published_at')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-def post_detail(request,pk):
+def post_detail(request, pk):
     post = get_object_or_404(Post,pk=pk)
-    return render(request, 'blog/post_list.html', {'post': post})
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 
 def post_new(request):
@@ -23,4 +26,18 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
