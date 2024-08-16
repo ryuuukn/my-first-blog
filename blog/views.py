607dbc360e1e6210c4403087626a74ab593424a5
ryuuukn <r.kazuta.24.3.1306@4nonome.com>
2024-08-16 23:45:59 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
-from django.utils import timezone
-from .models import Post
+from django.shortcuts import get_object_or_404
 
-def post_list(request):# 注意：published_dateでフィールドを作成している場合は、published_atとなっているところは全てpublished_dateにすること
-    posts = Post.objects.all().order_by('published_at')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
